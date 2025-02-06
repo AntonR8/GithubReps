652f9b74f2884940593396c2540f73d7552ba343
AntonR8 <129757394+AntonR8@users.noreply.github.com>
2025-02-06 16:53:17 +0300

    Initial Commit

---
+++ GithubReps/GithubRepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubRepsApp.swift
+//  GithubReps
+//
+//  Created by Антон Разгуляев on 06.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct GithubRepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
